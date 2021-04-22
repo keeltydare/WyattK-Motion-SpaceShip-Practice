d091c29a6e7ce849266681b883c53e646898b407
Keelty <keeltydare@gmail.com>
2021-04-22 10:21:40 -0400

    edits

--- dev/js/demo.js
+++ dev/js/demo.js
@@ -5,29 +5,36 @@ $(document).ready(function(){
 
   console.log('Page loaded');
 
-  //var mainTL = gsap.timeline();
-  //var tl = gsap.timeline();
+var cloud1 = $("#cloud-container img:nth-child(1)");
+var cloud2 = $("#cloud-container img:nth-child(2)");
+var cloud3 = $("#cloud-container img:nth-child(3)");
+var cloudArray = [cloud1,cloud2,cloud3]
+  var mainTL = gsap.timeline();
+  var tl = gsap.timeline();
   //defaults
 
+  var flameScale0 = gsap.utils.wrap([0.5,0.55,0.75]);
+  var flameScale1 = gsap.utils.wrap([0.5,0.55,0.75]);
+  var flameScale2 = gsap.utils.wrap([0.5,0.55,0.75]);
+  var flameScale3 = gsap.utils.wrap([0.5,0.55,0.75]);
+
 //first nested timeline animation
-  // function nestedTl(){
-  //   var tl = gsap.timeline();
-  //
-  //   tl.to(redBox ,{duration:1, x:400, ease:"none"})
-  //
-  //     .to(blueBox ,{duration:1, x:400, ease:"none"})
-  //
-  //     ;//timeline END
-  //
-  //     return tl;
-  //
-  // }
+  function cloudsTl(){
+     var tl = gsap.timeline();
+  
+     tl.to(cloudArray ,{duration:1, stagger:0.3, y:"+-700", ease:"expo.out"})
+  
+       ;//timeline END
+  
+       return tl;
+  
+   }
 
 
 
 
 //add main timeline
-mainTL.add(nestedTl())
+mainTL.add(cloudsTl())
 //.add(aniBoxOrange(), "-=1")
 
 ;//timeline END
